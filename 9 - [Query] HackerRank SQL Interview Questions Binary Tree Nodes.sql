bf02b6213ff6bdbc1b68d16df5b074ba483b4657
Karcan Ozbal <karcanozbal@hotmail.com.tr>
2024-10-01 21:38:20 +0300

    Update 9 - [Query] HackerRank SQL Interview Questions Binary Tree Nodes.sql

--- 9 - [Query] HackerRank SQL Interview Questions Binary Tree Nodes.sql
+++ 9 - [Query] HackerRank SQL Interview Questions Binary Tree Nodes.sql
@@ -1,11 +1,19 @@
+
+/*
+	• Root Node: The topmost node in a binary tree, without a parent.
+	• Inner Node: A node with at least one child.
+	• Leaf Node: A node with no children, found at the end of a branch.
+
+*/
 SELECT
 	N,
 	CASE
 		WHEN P IS NULL THEN 'Root'
-		WHEN N IN (SELECT P FROM BST) THEN 'Inner'
+		WHEN N IN (SELECT P FROM BST) THEN 'Insert'
 		ELSE 'Leaf'
 	END
 FROM
 	BST
 ORDER BY
 	N;
+
